12a7da07f6ad6ae5b3f55cac8ffae759a643e179
Paul Cuzner <pcuzner@redhat.com>
2016-08-22 11:13:00 +1200

    updated to integrate with the config object defined by the igw-ceph-ansible code

--- config/ceph.py
+++ config/ceph.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# get the lrbd.conf object and parse it
-# returning the object to the caller
 import random
 import subprocess
 import json
 
+from rtslib_fb import root
+from ceph_iscsi_gw.common import Config
 
 def add_rbd_maps(devices):
 
@@ -20,19 +20,61 @@ def add_rbd_maps(devices):
 class GatewayConfig(object):
     """ Configuration of the gateways, based on current state
     """
-    def __init__(self, runtime_opts, devices):
+    def __init__(self, logger, runtime_opts, devices):
 
+        self.gateways = []      # list of gateway names
+        self.diskmap = {}       # dict - device, pointing to client shortname
+        self.client_count = 0   # default count of clients connected to the gateway cluster
+
+        self.config = None      # config object from the rados config object
+        self.error = False      # error flag
+
+        # when setting the environment up, 1st allow for overrides
+        # TEST mode
         if runtime_opts.test:
+            # use some test names and make up some clients
             self.gateways = ['localhost','eric']
             self.diskmap = self._disk2client_mangler(devices)
-
+        #
+        # gateway name overrides
+        elif runtime_opts.gateways:
+            # use the gateway names provided
+            self.gateways = runtime_opts.gateways.split(',')
+        #
+        # default - use the config object stored by igw-ceph-ansible modules
         else:
-            self._read_conf()
+            # use the config object
+            if runtime_opts.config:
+                cfg_pool, cfg_obj = runtime_opts.config.split('/')
+                self.config = Config(logger, cfg_name=cfg_obj, pool=cfg_pool)
 
-        self.client_count = self._unique_clients()
+            gw_config = Config(logger)
+            if gw_config.error:
+                logger.debug("Unable to read the config object")
+                self.error = True
+            else:
+                # the config object is OK, so get the gateway information
+                self.gateways = gw_config.config['gateways'].keys()
+                if not self.gateways:
+                    self.error = True
 
+        if not self.error:
+            self.diskmap = self._get_mapped_disks()
+            self.client_count = self._unique_clients()
 
+    def _get_mapped_disks(self):
+        map = {}
+        lio_root = root.RTSRoot()
 
+        for m_lun in lio_root.mapped_luns:
+            client_iqn = m_lun.node_wwn
+            client_shortname = client_iqn.split(':')[-1]
+            image_name = m_lun.tpg_lun.storage_object.name
+            udev_path = m_lun.tpg_lun.storage_object.udev_path
+            dev_id = udev_path.split('/')[-1]
+            map[dev_id] = client_shortname
+
+        return map
 
     def _read_conf(self):
         self.gateways = []
@@ -50,14 +92,15 @@ class GatewayConfig(object):
         return map
 
     def _unique_clients(self):
+        # determine the number of unique client names in the disk -> client dict
         return len(set(self.diskmap.values()))
 
     def refresh(self):
         pass
 
 
-def get_gateway_info(opts, devices):
+def get_gateway_info(logger, opts, devices):
 
-    config = GatewayConfig(opts, devices)
+    config = GatewayConfig(logger, opts, devices)
 
     return config
