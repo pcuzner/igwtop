fdce7983934ae22e8d5121826241b0d1bfddc67c
Paul Cuzner <pcuzner@redhat.com>
2016-08-22 11:15:18 +1200

    updated to use a logging object instead of using print, and added --gateways and --config overrides

--- igwtop.py
+++ igwtop.py
@@ -2,6 +2,7 @@
 
 import argparse
 import time
+import sys
 from threading import Event
 
 from collectors.pcp_provider import PCPcollector
@@ -10,21 +11,30 @@ from config.local import get_device_info
 from config.ceph import get_gateway_info
 from igwtopUI.textmode import TextMode
 
+import logging
+
 
 def main():
 
     config = Config()
     config.opts = opts
     config.devices = get_device_info()
-    config.gateway_config = get_gateway_info(opts, config.devices)
+
+    config.gateway_config = get_gateway_info(logger, opts, config.devices)
+    if config.gateway_config.error:
+        # Problem determining the environment, so abort
+        logger.error("Error: Unable to determine the gateway configuration")
+        sys.exit(12)
+
     config.sample_interval = opts.interval
     collector_threads = []
     sync_point = Event()
     sync_point.clear()
+    logger.info("Attempting to open connections to pmcd daemons on the gateway nodes")
 
     # NB. interval must be a string, defaulting to 1 for testing
     for gw in config.gateway_config.gateways:
-        collector = PCPcollector(sync_point, host=gw, interval=config.sample_interval)
+        collector = PCPcollector(logger, sync_point, host=gw, interval=config.sample_interval)
 
         if collector.connected:
             collector.daemon = True
@@ -32,7 +42,7 @@ def main():
             collector_threads.append(collector)
         else:
             del collector
-            print "Error: Unable to connect to pmcd daemon on %s" % gw
+            logger.error("Error: Unable to connect to pmcd daemon on {}".format(gw))
 
     # Continue as long as we have at least 1 collector connected to a host's pmcd
     if len(collector_threads) > 0:
@@ -53,10 +63,14 @@ def main():
             # reset the terminal settings
             interface.reset()
     else:
-        print "Unable to continue, no pmcd's are available on the gateways to connect to"
+        logger.critical("Unable to continue, no pmcd's are available on the gateways to connect to")
 
 def get_options():
     parser = argparse.ArgumentParser(prog='igwtop', description='Show iSCSI gateway performance metrics')
+    parser.add_argument('-c', '--config', type=str,
+                        help='pool and object name holding the gateway config object (pool/object_name)')
+    parser.add_argument('-g', '--gateways', type=str,
+                        help='iscsi gateway server names (overrides the config object)')
     parser.add_argument('-i', '--interval', type=int, default=1,
                         help='monitoring interval (secs)', choices=range(1, 10))
     parser.add_argument('-d', '--debug', action='store_true', default=False,
@@ -64,13 +78,30 @@ def get_options():
     parser.add_argument('-m', '--mode', type=str, default='text',
                         choices=(['text']),
                         help='output mode')
-    parser.add_argument('-t', '--test', action='store_true', default=True,
+    parser.add_argument('-t', '--test', action='store_true', default=False,
                         help='run in test mode - i.e. without rados interaction')
+
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.5')
     opts = parser.parse_args()
     return opts
 
 if __name__ == '__main__':
     opts = get_options()
+
+    # define logging to the console
+    # set the format to just the msg
+    logger = logging.getLogger("igwtop")
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+
+    if opts.debug:
+        ch.setLevel(logging.DEBUG)
+    else:
+        ch.setLevel(logging.INFO)
+
+    fmt = logging.Formatter('%(message)s')
+    ch.setFormatter(fmt)
+    logger.addHandler(ch)
+
     # validate opts, then
     main()
